d3b4aaeecd242add6b13a71ae8345de6949bbf14
Wee Hui Min <areswee32@gmail.com>
2026-07-21 20:51:14 +0800

    Add number addition feature

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Repository
+
+This repository demonstrates Git version control, branching, merging, and remote collaboration.
---
+++ feature.py
@@ -0,0 +1,7 @@
+def add_numbers(first_number, second_number): 
+
+    """Return the sum of two numbers.""" 
+
+    return first_number + second_number 
+
+ 
